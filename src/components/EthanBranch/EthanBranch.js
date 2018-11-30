863ca8ee25d3dac1a9bfa2c6417e47aca9eb4e93
Ethan Sanders <ethanedu5@gmail.com>
2018-11-30 17:03:33 -0600

    added my name and a component

---
+++ src/components/EthanBranch/EthanBranch.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EthanBranch extends Component {
+    render() {
+        return (
+            <div>
+                Hello Word
+            </div>
+        );
+    }
+}
+
+export default EthanBranch;
